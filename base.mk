c05317b1a1d0c947a5553ad159233e12dacceb8a
Jim Nasby <Jim.Nasby@BlueTreble.com>
2015-12-26 13:26:04 -0600

    Add target to list all targets

--- base.mk
+++ base.mk
@@ -124,6 +124,13 @@ dist-only:
 .PHONY: forcedist
 forcedist: forcetag dist
 
+# Target to list all targets
+# http://stackoverflow.com/questions/4219255/how-do-you-get-the-list-of-targets-in-a-makefile
+.PHONY: no_targets__ list
+no_targets__:
+list:
+	sh -c "$(MAKE) -p no_targets__ | awk -F':' '/^[a-zA-Z0-9][^\$$#\/\\t=]*:([^=]|$$)/ {split(\$$1,A,/ /);for(i in A)print A[i]}' | grep -v '__\$$' | sort"
+
 # To use this, do make print-VARIABLE_NAME
 print-%	: ; $(info $* is [${$*}])@echo -n
 
