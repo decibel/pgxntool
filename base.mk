0c84d437270bec83edc5a3c2341f4f500e31776b
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-01-15 16:01:08 -0600

    Improve print- recipe

--- base.mk
+++ base.mk
@@ -140,7 +140,8 @@ list:
 	sh -c "$(MAKE) -p no_targets__ | awk -F':' '/^[a-zA-Z0-9][^\$$#\/\\t=]*:([^=]|$$)/ {split(\$$1,A,/ /);for(i in A)print A[i]}' | grep -v '__\$$' | sort"
 
 # To use this, do make print-VARIABLE_NAME
-print-%	: ; $(info $* is $(flavor ${$*}) variable set to [${$*}])@echo -n
+print-%	: ; $(info $* is $(flavor $*) variable set to "$($*)") @true
+
 
 #
 # subtree sync support
