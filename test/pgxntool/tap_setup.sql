2f11ef0007142f86a889e440afdeb48368077cdd
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-11-03 19:57:53 -0500

    Support 9.2

--- test/pgxntool/tap_setup.sql
+++ test/pgxntool/tap_setup.sql
@@ -1,10 +1,19 @@
 \i test/pgxntool/psql.sql
 
 SET client_min_messages = WARNING;
-CREATE SCHEMA IF NOT EXISTS tap;
+
+DO $$
+BEGIN
+IF NOT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname='tap') THEN
+  CREATE SCHEMA tap;
+END IF;
+END$$;
+
 SET search_path = tap, public;
 CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
 SET client_min_messages = NOTICE;
 \pset format unaligned
 \pset tuples_only true
 \pset pager
+
+-- vi: expandtab ts=2 sw=2
